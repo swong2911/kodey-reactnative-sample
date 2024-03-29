909708ed441a31abddc06b4a98fba718fe226970
swong2911 <165393318+swong2911@users.noreply.github.com>
2024-03-29 11:08:58 -0400

    Add SignupScreen component

---
+++ enlytai-react-sample-project/src/component/SignupScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const SignupScreen = () => {
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const isFormEmpty = username === '' || email === '' || password === '';
+
+  return (
+    <div style={{ margin: '20px' }}>
+      <h2>Signup</h2>
+      <form>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit" disabled={isFormEmpty}>Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupScreen;
